5cf1a0c69b9889a1680a533078de15c822becc5a
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2023-09-05 08:40:35 -0300

    feat: redirect to post path if sign in succeed

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,33 +1,50 @@
 'use client'
 
 import { SignActionPage } from "@/components/SignActionPage"
+import { useSignIn } from "@/hooks/auth/useSignIn";
 import { useGetInput } from "@/hooks/useGetInput"
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function SignInPage() {
-  const { inputs, handleGetInput } = useGetInput()
+  const { inputs, handleGetInput } = useGetInput();
+  const { signIn, error } = useSignIn();
+  const router = useRouter();
 
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const { email, password } = inputs.current;
+    signIn({ email, password });
+    if(!error.exists) router.push('/posts')
+  }
 
   return(
     <SignActionPage>
-      <form onSubmit={e => { e.preventDefault();  console.log(inputs)}}>
-        <div>
-          <label htmlFor="signInEmail">E-mail</label>
+      
+      <form onSubmit={handleSubmit}>
+        <small className="text-red-400 text-center block">{error.exists && error.message}</small>
+        <div className="mb-4">
+          <label className="block text-white" htmlFor="signInEmail">E-mail</label>
           <input
             onChange={e => handleGetInput({key: 'email', value: e.target.value})}
             id="signInEmail" 
-            type="email" 
+            type="email"
+            className="w-full rounded-md " 
           />
         </div>
-        <div>
-          <label htmlFor="signInPassword">Senha</label>
+        <div className="mb-5">
+          <label className="block text-white" htmlFor="signInPassword">Senha</label>
           <input 
             onChange={e => handleGetInput({key: 'password', value: e.target.value})}
             id="signInPassword"
             type="password" 
+            className="w-full rounded-sm"
           />
         </div>
 
-        <button type="submit">Sign In</button>
+        <button type="submit" className="w-full bg-slate-900 py-1 rounded-md text-white">Sign In</button>
+        <small className="block mt-1 text-center"> caso ainda não possua conta, 
+          <Link href="/signUp" className="text-blue-200 underline ml-1">faça seu cadastro aqui</Link></small>
       </form>
     </SignActionPage>
   )
