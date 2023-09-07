19eab2267abeaa380c1784b41e960e0374eb20ac
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2023-09-07 14:42:07 -0300

    chore: allow image url

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '64.media.tumblr.com',
+        pathname: '/**/**'
+      }
+    ]
+  }
+}
 
 module.exports = nextConfig
