67d8c46872e0e14118d051e9a7039cbff0dc4c6e
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2023-09-12 15:26:25 -0300

    feat: real time database hook

---
+++ src/hooks/db/useRealtime.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { realtimeDb } from "@/lib/firebase/realtimeDb";
+import {  onValue, push, ref } from "firebase/database";
+import React from "react";
+
+export function useRealtime() {
+  const [items, setItems] = React.useState<any[]>();
+  
+  function postItem(userId:string, product: any) {
+    const productRef = ref(realtimeDb, `posts/`)
+    push(productRef, { ...product, ownerId: userId })
+    setItems(product)
+  }
+
+  function observeChangesOn(reference:string) {
+    const itemsRef = ref(realtimeDb, reference);
+
+    onValue(itemsRef, snapshot => {
+      const items = getValuesFromSnapshot(snapshot.val())
+      setItems(items);
+    })
+  }
+
+  function getValuesFromSnapshot(snapshotValue: object) {
+    return Object.values(snapshotValue)
+  }
+
+  return { observeChangesOn , postItem, items}
+}
+
+
