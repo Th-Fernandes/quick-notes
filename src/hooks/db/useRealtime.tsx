aaf6e29520b0b4895ba756e8f927d303d4a3b1b1
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2023-09-13 09:23:36 -0300

    refactor: using generic names

--- src/hooks/db/useRealtime.tsx
+++ src/hooks/db/useRealtime.tsx
@@ -7,26 +7,26 @@ import { onValue, push, ref } from "firebase/database";
 export function useRealtime() {
   const [items, setItems] = React.useState<any[]>();
   
-  function postItem(userId:string, product: any) {
-    const productRef = ref(realtimeDb, `posts/`)
-    push(productRef, { ...product, ownerId: userId })
-    setItems(product)
+  function postItem(userId:string, item: any) {
+    const productRef = ref(realtimeDb, `posts/`);
+    push(productRef, { ...item, ownerId: userId });
+    setItems(item);
   }
 
   function observeChangesOn(reference:string) {
     const itemsRef = ref(realtimeDb, reference);
 
     onValue(itemsRef, snapshot => {
-      const items = getValuesFromSnapshot(snapshot.val())
+      const items = getValuesFromSnapshot(snapshot.val());
       setItems(items);
     })
   }
 
   function getValuesFromSnapshot(snapshotValue: object) {
-    return Object.values(snapshotValue)
+    return Object.values(snapshotValue);
   }
 
-  return { observeChangesOn , postItem, items}
+  return { observeChangesOn , postItem, items };
 }
 
 
